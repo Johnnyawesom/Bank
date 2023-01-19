dabc181eb05c09b7e1dbbcbca97a950725182e5c
Johnny Langskov Hansen <johnny@vkdata.dk>
2023-01-19 19:02:26 +0100

    [IMP] Added a little to the bank system

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_1
+{
+    interface IBank
+    {
+        public static int _bankcounter = 1;
+    }
+
+    class Bank : IBank
+    {
+        int id;
+        string name;
+        List<IAccount> accountList = new List<Iaccount>
+    }
+        
+
+}

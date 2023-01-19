0d454e8b6efdb07a84bd1b77da77d5b1577abdb6
Johnny <johnny@vkdata.dk>
2023-01-19 19:54:04 +0100

    [ADD] added more stuff to bank program

--- ConsoleApp1/ConsoleApp1/Bank.cs
+++ ConsoleApp1/ConsoleApp1/Bank.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using bank_1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,14 +18,53 @@ namespace Bank_1
         string name;
         List<IAccount> accountList = new List<IAccount>();
 
+        // Constructor
         public Bank(string _bankName)
         {
             this.id = IBank._bankcounter++; ;
             this.name = _bankName;
-        
+
+        }
+
+        // Properties
+        public string Name {
+            get { return this.id; }
+            get { this.name = value; }
+        }
+        public int ID { get { return this.id } }
+
+        //Methodes
+        public IAccount CreateAccount(string name, AccountType accountType)
+        {
+            IAccount newAccount;
+            switch (accountType)
+            {
+                case AccountType.CheckingAccount(name);
+                    this.accountList.Add(newAccount);
+                    return newAccount;
+                case AccountType.savingsAccount:
+                    this.accountList.Add(newAccount);
+                    return newAccount;
+                case AccountType.consumerAccount:
+                    this.accountList.Add(newAccount);
+                    return newAccount;
+                default
+                    return null;
+            }
         }
 
     }
-        
+
+    public int AddInterests()
+    {
+        foreach (IAccount account in accountList)
+        {
+         account.AddInterests();
+        }
+        return accountList.Count;
+    }
+
+    //TODO: Implement Checks when deposet
+    public bool Withdraw 
 
 }

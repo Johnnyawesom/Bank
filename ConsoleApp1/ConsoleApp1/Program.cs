95e180882e61128ba94827b6cf5c0f0f493bc3de
Johnny <johnny@vkdata.dk>
2023-01-19 21:44:21 +0100

    [ADD] added new stuff and files

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Bank_1
+{
+    class Program
+    {
+        static List<Bank> Banks = new List<Bank>();
+        //creating default Bank
+        static Bank bank = new Bank("snydbank");
+
+        static void Main(string[] args)
+        {
+            bool KeepRunning = true;
+            do
+            {
+                int selectedMenuoption = ConsoleHelper.MultipleChoice(true, "Opret konto")
+            }
+        }
+    }
+}
